7afb237f3e69897170cbe8a9adb054ed5cf03080
Maxxel <108078733+Jinaan@users.noreply.github.com>
2024-02-13 00:33:52 +0700

    Update main.py

--- VAI Downloader/main.py
+++ VAI Downloader/main.py
@@ -1,24 +1,16 @@
 from parts.ChromeOpener import opener
-from parts.ManageCollection import ManageCollection
+from parts.firstTimeWait import FirstTimeWAit
 from parts.DownloadModel import DownloadModel
-from parts.configAccess import getConfig
-config = getConfig()
+from parts.ManageCollection import ManageCollection
 
-FIRSTTIME = config["FIRST_TIME"]
+
 
 def main():
     driver = opener()
     driver.get("https://civitai.com/")
 
-    if FIRSTTIME:
-        import time
-        print("Please login to your account within 500 seconds")
-        given_time = 500
-        for i in range(given_time):
-            time.sleep(1)
-            countdown = str(given_time - i).zfill(3)
-            print(countdown, end="\r")
-        print("Time's up")
+    FirstTimeWAit()
+    
     CardData = ManageCollection(driver)
     
     DownloadModel(driver, CardData)
