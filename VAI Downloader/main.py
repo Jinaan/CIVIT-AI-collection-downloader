8f44b15725ed8a1baa308a4c585cd5121427a22f
Maxxel <108078733+Jinaan@users.noreply.github.com>
2024-02-15 16:28:59 +0700

    Update main.py

--- VAI Downloader/main.py
+++ VAI Downloader/main.py
@@ -2,10 +2,11 @@ from parts.ChromeOpener import opener
 from parts.firstTimeWait import FirstTimeWAit
 from parts.DownloadModel import DownloadModel
 from parts.ManageCollection import ManageCollection
-
+from parts.CheckingFile import checkingFile
 
 
 def main():
+    checkingFile()
     driver = opener()
     driver.get("https://civitai.com/")
 
@@ -19,6 +20,8 @@ def main():
     print("done")
     
     driver.quit()
+    checkingFile()
+    
 
 
 
